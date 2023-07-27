7315ac3e4d07c78842cf70d9268e5609876ce152
Jun Beom Park <94429684+JunBeoPark@users.noreply.github.com>
2023-07-27 16:48:04 +0900

    Update MenuItem.kt

--- app/src/main/java/com/example/kiosk/MenuItem.kt
+++ app/src/main/java/com/example/kiosk/MenuItem.kt
@@ -1,5 +1,5 @@
 package com.example.kiosk;
 
-open class MenuItem(val id: Int, val name: String, val price: Double, val category: String,val description: String) {
-        fun display() = "$name | W $price"
+open class MenuItem(val id: Int, val name: String, val price: Double, val category: String,val description: String, val hotIce: String) {
+    fun display() = "$name | W $price"
 }
